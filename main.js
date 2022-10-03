3e8ec025b9f9eb489414e848703521e00b3a29f2
jakovid <jacobk.dowdle@gmail.com>
2022-10-03 11:53:20 +0800

    wowowowow I added the functions to update temp humidity and wind, and to switch between f and c

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const img = document.getElementById('weatherImg');
-let weatherImgType = 'rain';
 let temperature = '';
-let percipitation = '';
+let precipitation = 'Percipitation: idk how to calc lol';
 let humidity = '';
 let wind = '';
 let city = 'Cincinnati';
@@ -9,24 +8,61 @@ let state = 'OH';
 let country = 'US';
 let locationDayTime = 'Sunday, 4:30PM';
 let locationWeatherType = 'Mostly sunny';
-let longitude = '';
-let latitude = '';
+let longitude = -84.5124602;
+let latitude = 39.1014537;
+let unitType = 'metric';
 
+function addEventListeners() {
+    let celciusButton = document.getElementById('celsius');
+    let fahrenheitButton = document.getElementById('fahrenheit');
 
-function populateLocationInformation() {
+    celciusButton.addEventListener('click', e => {
+        changeUnitType('metric');
+        console.log('click');
+        displayWeatherInformation();
+    });
+    fahrenheitButton.addEventListener('click', e => {
+        changeUnitType('imperial');
+        displayWeatherInformation();
+    })
+}
+
+function changeUnitType(selectedUnitType) {
+    unitType = selectedUnitType;
+}
+
+async function displayWeatherInformation() {    
+    await populateLocationInformation();
+    getWeatherImg();
+}
+
+async function populateLocationInformation() {
+
+    await getWeatherInformation();
+
+    let temperatureDisplay = document.getElementById('degreeNumber');
+    let precipitationDisplay = document.getElementById('precipitation');
+    let humidityDisplay = document.getElementById('humidity');
+    let windDisplay = document.getElementById('wind');
     let locationNameBox = document.getElementById('locationName');
     let locationDayTimeBox = document.getElementById('locationDayTime');
     let locationWeatherTypeBox = document.getElementById('locationWeatherType');
 
+    temperatureDisplay.innerHTML = temperature;
+    precipitationDisplay.innerHTML = precipitation;
+    humidityDisplay.innerHTML = humidity;
+    windDisplay.innerHTML = wind;
     locationNameBox.innerHTML = city + ', ' + state + ', ' + country;
     locationDayTimeBox.innerHTML = locationDayTime;
     locationWeatherTypeBox.innerHTML = locationWeatherType;
+    console.log(temperatureDisplay);
+    console.log('hahhhh');
     
 }
 
 async function getWeatherImg() {
     img.src = '#';
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=kvwilimQvZM1Dlh0EkhG1ZiEEEn9zkii&s=${weatherImgType}`, { mode: 'cors' })
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=kvwilimQvZM1Dlh0EkhG1ZiEEEn9zkii&s=${locationWeatherType}`, { mode: 'cors' })
     const weatherImgData = await response.json();
     img.src = weatherImgData.data.images.original.url;
 }
@@ -41,12 +77,15 @@ async function getLongitudeLatitude() {
 }
 
 async function getWeatherInformation() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5dd670ae6c88bc100c0dc5e21791aef5`);
+    await getLongitudeLatitude();
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=5dd670ae6c88bc100c0dc5e21791aef5&units=${unitType}`);
     const weatherData = await response.json();
     console.log(weatherData);
+    temperature = weatherData.main.temp;
+    humidity = 'Humidity: ' + weatherData.main.humidity+'%';
+    wind = 'Wind: ' + weatherData.wind.speed +`${unitType == 'metric' ? 'km/h' : 'mph'}`;
+    locationWeatherType = weatherData.weather[0].main;
 }
 
-getWeatherImg();
-populateLocationInformation();
-getLongitudeLatitude();
-getWeatherInformation();
+addEventListeners();
+displayWeatherInformation();
