cb7bc24bf459f36cf7b412ec31670770eaba4d8f
jakovid <jacobk.dowdle@gmail.com>
2022-10-03 11:06:24 +0800

    changed APID

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const img = document.getElementById('weatherImg');
 let weatherImgType = 'rain';
+let temperature = '';
+let percipitation = '';
+let humidity = '';
+let wind = '';
 let city = 'Cincinnati';
 let state = 'OH';
 let country = 'US';
@@ -7,6 +11,7 @@ let locationDayTime = 'Sunday, 4:30PM';
 let locationWeatherType = 'Mostly sunny';
 let longitude = '';
 let latitude = '';
+
 
 function populateLocationInformation() {
     let locationNameBox = document.getElementById('locationName');
@@ -21,21 +26,27 @@ function populateLocationInformation() {
 
 async function getWeatherImg() {
     img.src = '#';
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=kvwilimQvZM1Dlh0EkhG1ZiEEEn9zkii&s=${weatherImgType}`, { mode: 'cors' })
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyID}&s=${weatherImgType}`, { mode: 'cors' })
     const weatherImgData = await response.json();
     img.src = weatherImgData.data.images.original.url;
 }
 
 async function getLongitudeLatitude() {
-    city = document.getElementById('cityLocationInput').value;
-    state = document.getElementById('stateLocationInput').value;
-    country = document.getElementById('countryLocationInput').value;
-
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=5dd670ae6c88bc100c0dc5e21791aef5`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=${weatherID}`);
     const locationWeatherData = await response.json();
     latitude = locationWeatherData[0].lat;
     longitude = locationWeatherData[0].lon;
+    console.log(longitude);
+    console.log(latitude);
+}
+
+async function getWeatherInformation() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherID}`);
+    const weatherData = await response.json();
+    console.log(weatherData);
 }
 
 getWeatherImg();
 populateLocationInformation();
+getLongitudeLatitude();
+getWeatherInformation();
